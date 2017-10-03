b42ab45559460c7eef20f1bc15d5455292f770bf
Duc Dao <ducdh1@smartosc.com>
2017-10-03 19:00:13 +0700

    Add sample tests

---
+++ Test/Unit/Block/Product/View/GalleryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Unet\ProductVideo\Test\Unit\Block\Product\View;
+
+/**
+ * Class GalleryTest
+ * @package Unet\ProductVideo\Test\Unit\Block\Product\View
+ */
+class GalleryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * testGetIdentities
+     */
+    public function testGetIdentities()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ Test/Unit/Block/ProductVideo/View/GalleryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Unet\ProductVideo\Test\Unit\Block\ProductVideo\View;
+
+/**
+ * Class GalleryTest
+ * @package Unet\ProductVideo\Test\Unit\Block\ProductVideo\View
+ */
+class GalleryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * testGetIdentities
+     */
+    public function testGetIdentities()
+    {
+        $this->assertTrue(true);
+    }
+}
